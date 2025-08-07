5b407865e303fb6b66c4e7bb64b53c2f18759f00
andres0615 <bafdurango@hotmail.com>
2025-08-07 00:56:53 -0500

    feat: se agrega .htaccess para apuntar a la carpeta public

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     @if(app()->environment('production'))
-    <base href="/~usuario24/public/">
+    <base href="/~usuario24/">
     @else
     <base href="/"> <!-- Importante para Angular routing -->
     @endif
